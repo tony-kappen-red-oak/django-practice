beba52c3eb6cbf74a47eac60587926e9379fcdc1
Tony Kappen <tony.kappen@redoaktechnologies.ca>
2021-01-06 09:07:57 -0500

    deploying

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -74,12 +74,12 @@ WSGI_APPLICATION = 'todo_list.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
